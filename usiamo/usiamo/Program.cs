e5cbd776f1bd508fa27b40337935e348ced7c434
SebasCastaneda <carlossebastian.sotocastaneda.stud@ispascalcomandini.it>
2022-02-09 17:13:37 +0100

    2 versione

---
+++ usiamo/usiamo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usiamo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
